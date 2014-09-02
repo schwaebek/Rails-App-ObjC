a05e270d33f32a8e2f9d2d57434ff1e4be3062a6
schwaebek <schwaebek@Katlyns-MacBook-Pro.local>
2014-09-02 14:15:23 -0400

    Rails App ObjC - Initial commit

---
+++ Rails App ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rails App ObjC
+//
+//  Created by Katlyn Schwaebe on 9/2/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Rails App ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Rails App ObjC
+//
+//  Created by Katlyn Schwaebe on 9/2/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
